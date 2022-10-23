fcac196becf029bde9805f9ca9ccbee2229fe7c6
Tay Kim <taykimpro@gmail.com>
2022-10-22 17:09:32 -0700

    ..

--- api/word-quiz.py
+++ api/word-quiz.py
@@ -1,4 +1,8 @@
-import requests
+from contextlib import nullcontext
+from flask import Blueprint, jsonify  # jsonify creates an endpoint response object
+from flask_restful import Api, Resource # used for REST API building
+import requests  # used for testing 
+import time
 
 url = "https://twinword-word-association-quiz.p.rapidapi.com/type1/"
 
