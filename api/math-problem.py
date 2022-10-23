ba130584f8daf3871dfc35cb977ad1850afc8ac5
Tay Kim <taykimpro@gmail.com>
2022-10-22 17:08:46 -0700

    ..

--- api/math-problem.py
+++ api/math-problem.py
@@ -1,4 +1,8 @@
-import requests
+from contextlib import nullcontext
+from flask import Blueprint, jsonify  # jsonify creates an endpoint response object
+from flask_restful import Api, Resource # used for REST API building
+import requests  # used for testing 
+import time
 
 url = "https://random-math-problem.p.rapidapi.com/random-problem"
 
