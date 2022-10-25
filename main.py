26c8cc500ebd28031498162720dbd0217762ffb3
Tay Kim <taykimpro@gmail.com>
2022-10-25 00:47:09 -0700

    quiz api

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from flask import render_template  # import render_template from "public" flask 
 from __init__ import app  # Definitions initialization
 from api.joke import joke_api # Blueprint import api definition
 from api.covid import covid_api # Blueprint import api definition
-from api.quiz import quiz_api # Blueprint import api definition
+from api.math import math_api # Blueprint import api definition
 
 from bp_projects.projects import app_projects # Blueprint directory import projects definition
 
 app.register_blueprint(joke_api) # register api routes
 app.register_blueprint(covid_api) # register api routes
-app.register_blueprint(quiz_api)
+app.register_blueprint(math_api) # register api routes
 
 app.register_blueprint(app_projects) # register api routes
 
