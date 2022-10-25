20d01ff183ef62e66b545c717cc47b61fb2bf956
Tay Kim <taykimpro@gmail.com>
2022-10-24 17:54:16 -0700

    ..

---
+++ api/quiz.js
@@ -0,0 +1,11 @@
+url = "https://opentdb.com/api.php?amount=10&category=19&difficulty=medium&type=multiple"
+
+console.log(url[question])
+
+//querystring = {"type":"html"}
+
+//response = requests.request("GET", url, params=querystring)
+
+//print(response.text)
+
+
