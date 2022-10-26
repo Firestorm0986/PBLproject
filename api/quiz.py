6a29871a7d33b507d1b2ef770b298a7aebafd3c9
Lazershep <Lazershep@gmail.com>
2022-10-26 14:31:04 -0700

    Oct 26 2

--- api/quiz.py
+++ api/quiz.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify  # jsonify creates an endpoint response object
+from flask import Blueprint, jsonify  # jsonify creates an endpoint response object.
 from flask_restful import Api, Resource # used for REST API building.
 import requests  # used for testing.
 
