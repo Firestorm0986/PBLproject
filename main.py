90fe741d874c563c3b857ed4a18ec3caac7377e8
Tay Kim <taykimpro@gmail.com>
2022-10-31 10:16:53 -0700

    ..

--- main.py
+++ main.py
@@ -4,12 +4,13 @@ from flask import render_template  # import render_template from "public" flask 
 from __init__ import app  # Definitions initialization
 from api.joke import joke_api # Blueprint import api definition
 from api.covid import covid_api # Blueprint import api definition.
+from api.SATquiz import SATquiz_api
 
 from bp_projects.projects import app_projects # Blueprint directory import projects definition
 
 app.register_blueprint(joke_api) # register api routes
 app.register_blueprint(covid_api) # register api routes
-
+app.register_blueprint(SATquiz_api)
 
 app.register_blueprint(app_projects) # register api routes
 
